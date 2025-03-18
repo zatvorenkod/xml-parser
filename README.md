d5ffe948db2e37807d9aa566fec0b4acf8023269
Stefan N <9420404-stefan_najdovski@users.noreply.gitlab.com>
2025-03-18 17:03:25 +0100

    read a file

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple xml parser written in C
 # Credits/Resources used to learn
 
 - [Intro to XML](https://www.stat.berkeley.edu/~nolan/stat133/Fall05/lectures/introXML.pdf).
-
+- [Processing XML Docs](https://www.cs.sjsu.edu/faculty/pearce/modules/lectures/web/xml/)
 
 # How to compile
 - You need clang/gcc (test on macos)
--- src/main.c
+++ src/main.c
@@ -1,15 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//First I need is to read a file, to parse something duh!
+//https://courses.cs.washington.edu/courses/cse303/09au/cfileio.pdf
+int readFile(const char* path){
+
+    FILE *fp = fopen(path,"r");
+    if(fp == NULL){
+        printf("reading %s fail",path);
+    }
+
+    char c;
+
+    while((c=fgetc(fp))!=EOF){
+        printf("%c",c);
+    }
+
+    fclose(fp);
+
+}
 
 int main(int argc, char **argv){
 
     if(argc > 1){
-        printf(">1 arguments\n");
+        printf("> 1 arguments\n");
         //printf("arg:%s",argv[1]);
+        readFile(argv[1]);
     }else{
         printf("no args \n");
     }
+
 
     printf("Hello from C!\n");
 
