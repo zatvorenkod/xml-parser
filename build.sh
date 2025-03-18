7e982c8467ba593ac6e280dd786bba81b6c6a1ca
Stefan N <9420404-stefan_najdovski@users.noreply.gitlab.com>
2025-03-18 16:34:48 +0100

    init hello world

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+clang -Wall -Wextra -g -O0 src/main.c -o xml_parser
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello from C!\n");
+
+    return EXIT_SUCCESS;
+}
